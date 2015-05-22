edc8c0e5cf4a3d5aee81463a432b349e70d41da0
Michael Pastore <bgrva@hotmail.com>
2015-05-22 15:14:31 -0400

    clean up

--- x.Graph.Core/Default/Edge.cs
+++ x.Graph.Core/Default/Edge.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace x.Graph.Core.Default
+﻿namespace x.Graph.Core.Default
 {
-    class Edge
+    public class Edge
     {
     }
 }
