091b8a6a0b24e490c0c0dbabd81e1cacc492c4ba
Vekyllis <kevin.hulsing@gmail.com>
2015-05-22 22:27:13 -0400

    Adding node capabilities to Graph object.

--- x.Graph.Core/Default/Node.cs
+++ x.Graph.Core/Default/Node.cs
@@ -1,6 +1,55 @@
-﻿namespace x.Graph.Core.Default
+﻿using System;
+
+namespace x.Graph.Core.Default
 {
-    public class Node
+    public class Node<T> : INode<T>
     {
+        public System.Collections.Generic.IEnumerable<IEdge<T>> IncomingEdges
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Collections.Generic.IEnumerable<IEdge<T>> OutgoingEdges
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Collections.Generic.IEnumerable<INode<T>> Predecessors
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Collections.Generic.IEnumerable<IEdge<T>> SelfLoops
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public System.Collections.Generic.IEnumerable<INode<T>> Successors
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public T UniqueId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
